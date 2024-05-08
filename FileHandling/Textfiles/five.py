d4ee7695efea3f969f985b202c9cebd9547c7505
USER 1 <gchkumar999@gmail.com>
2024-05-08 18:46:27 +0530

    prat

--- FileHandling/Textfiles/five.py
+++ FileHandling/Textfiles/five.py
@@ -1,5 +1,7 @@
 #append text into emp.txt file 
 
+
+
 fp=open('emp.txt','a')
 data = "Hi hello"
 fp.write(data)
---
+++ FileHandling/Textfiles/six.py
@@ -0,0 +1,7 @@
+
+
+
+fp=open('prat.txt','a')
+data = "Hi hello"
+fp.write(data)
+fp.close()
