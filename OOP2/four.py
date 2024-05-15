ad696a32681e2386e3ba6158d68b6e0a62a076cf
USER 1 <gchkumar999@gmail.com>
2024-05-15 23:46:10 +0530

    prat

---
+++ OOP2/four.py
@@ -0,0 +1,19 @@
+class Test:
+    a=10  #static variable
+    def m1(self):
+        self.b=20
+    def m2(self):
+        self.c=30
+    def m3(self):
+        self.d=40
+    
+t1=Test()
+t1.m1()
+
+t2=Test()
+t2.m2()
+t2.m3()
+
+
+print(t1.__dict__)
+print(t2.__dict__)
