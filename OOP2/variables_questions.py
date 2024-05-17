f8a71650a4a88b80f8a024cb3d511bb4b390f511
USER 1 <gchkumar999@gmail.com>
2024-05-17 23:24:05 +0530

    prat

--- OOP2/variables_questions.py
+++ OOP2/variables_questions.py
@@ -5,11 +5,18 @@
 # user can define an instance of a particular class  as an individual object.
 
 
+# ------------------------------------------------------------------------------------
 
 #what is static variable
 
 
+
 # static variables are those variables that are declared within a given class but outside the object in the class
+
+
+
+
+# ------------------------------------------------------------------------------------
 
 
 #what is local variable 
