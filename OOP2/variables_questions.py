d0d666c6868ac608c998a676b15b086fa554a840
USER 1 <gchkumar999@gmail.com>
2024-05-17 23:05:14 +0530

    prat

---
+++ OOP2/variables_questions.py
@@ -0,0 +1,14 @@
+#what is instance variable
+
+
+# Instance are object of a class in python ,
+# user can define an instance of a particular class  as an individual object.
+
+
+
+#what is static variable
+
+
+
+
+#what is local variable 
