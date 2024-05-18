d69cf5b8e668104c197abe22c56267568252bacd
USER 1 <gchkumar999@gmail.com>
2024-05-18 23:08:43 +0530

    prat

---
+++ OOP2/eight.py
@@ -0,0 +1,39 @@
+# class Test:
+    
+#     def m1(self):
+#         self.a=10
+#     @classmethod
+#     def m2(cls):
+#         cls.b=20
+#     @staticmethod
+#     def m3():
+#         c=30
+
+# t1=Test()
+# t1.m1()
+# t1.m2()
+# t1.m3()
+
+# print(t1.__dict__)
+# print(Test.__dict__)
+
+class Test():
+      
+    def m1(self):
+        self.a=100
+    @classmethod
+    def m2(cls):
+        cls.b=200
+    @staticmethod
+    def m3():
+        c=300
+
+t1=Test()
+t1.m1()
+t1.m2()
+t1.m3()
+
+print(t1.__dict__)
+print(Test.__dict__)
+
+  
