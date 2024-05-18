e83c27d3ff0e8c2348aee1e8dbf88ddf2eb09f66
USER 1 <gchkumar999@gmail.com>
2024-05-18 23:38:44 +0530

    prat

---
+++ OOP2/ten.py
@@ -0,0 +1,24 @@
+class Test():
+      
+    def m1(self):
+        self.a=30
+    @classmethod
+    def m2(cls):
+        cls.b=50
+    @classmethod
+    def m3(cls):
+       cls.c=100
+    @staticmethod
+    def m4():
+       d=400
+
+t1=Test()
+t1.m1()
+t1.m2()
+t1.m3()
+
+print(t1.__dict__)
+print(Test.__dict__)
+
+
+  
